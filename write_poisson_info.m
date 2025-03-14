591a503b646c308594cf02d449dde68a18fee9b5
snytav <snytav@gmail.com>
2025-03-14 18:13:38 +0200

    1st

---
+++ write_poisson_info.m
@@ -0,0 +1,12 @@
+function [] = write_poisson_info(x,v,f,E,time)
+rho = trapz(v, f, 2);
+fn= sprintf("x_%10.3e.txt",time);
+
+dlmwrite(fn,x,'delimiter','\n','precision',15);
+fn= sprintf("rho_%10.3e.txt",time);
+dlmwrite(fn,rho,'delimiter','\n','precision',15);
+fn= sprintf("E_%10.3e.txt",time);
+dlmwrite(fn,E,'delimiter','\n','precision',15);
+
+
+end
